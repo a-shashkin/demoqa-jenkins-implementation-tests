09415b19bc5fc71b8ece140380ddcdfe18f0aafc
Alexander Shashkin <caezar1993@gmail.com>
2022-07-27 20:11:42 +0400

    Added scripts to remove ads, because they mess up tests

--- src/test/java/com/simbirsoft/pages/RegistrationPage.java
+++ src/test/java/com/simbirsoft/pages/RegistrationPage.java
@@ -10,8 +10,7 @@ import io.qameta.allure.Step;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationPage {
 
@@ -40,6 +39,11 @@ public class RegistrationPage {
     public RegistrationPage openPage() {
         open("/automation-practice-form");
         formTitle.shouldHave(text(FORM_TITLE));
+        if ($(".adsbygoogle").is(visible)) {
+            executeJavaScript(
+                    "$('.adsbygoogle').hide();"
+            );
+        }
         return this;
     }
 
--- src/test/java/com/simbirsoft/tests/DemoQAElementPresenceTests.java
+++ src/test/java/com/simbirsoft/tests/DemoQAElementPresenceTests.java
@@ -1,13 +1,10 @@
 package com.simbirsoft.tests;
 
-import com.codeborne.selenide.Condition;
 import com.simbirsoft.pages.RegistrationPage;
 import io.qameta.allure.*;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Selenide.$;
 
 @Feature("Страница регистрации студента DemoQA")
 @Owner("Alexander Shashkin")
